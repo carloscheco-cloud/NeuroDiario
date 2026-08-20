a4f91c49dd8ef6aa64063a1eb38e1359bfe40641
carloscheco-cloud <carloscheco@gmail.com>
2026-08-20 15:55:12 -0400

    feat(neurodata): export article text enricher

--- neurodiario/neurodata/collectors/__init__.py
+++ neurodiario/neurodata/collectors/__init__.py
@@ -1,4 +1,5 @@
+from .article_text import ArticleTextEnricher
 from .serper import SerperCollector
 from .youtube import YouTubeCollector
 
-__all__ = ["SerperCollector", "YouTubeCollector"]
+__all__ = ["ArticleTextEnricher", "SerperCollector", "YouTubeCollector"]
